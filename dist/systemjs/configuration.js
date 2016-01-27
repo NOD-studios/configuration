6cae9eb12ccdcc59022cc7a7b146c750667fd2af
Furkan Tunalı <furkan@nod.st>
2016-01-27 04:52:12 +0200

    Add a default configured property

--- dist/systemjs/configuration.js
+++ dist/systemjs/configuration.js
@@ -22,7 +22,10 @@ System.register(['@nod/environment', './merger'], function (_export, _context) {
 
         _classCallCheck(this, Configuration);
 
-        var merger = new Merger(this, new Environment().config, options);
+        this.configured = false;
+        var merger = new Merger(this, new Environment().config, options, {
+          configured: true
+        });
         Object.assign(this, merger.options);
         return this;
       });
@@ -31,4 +34,4 @@ System.register(['@nod/environment', './merger'], function (_export, _context) {
     }
   };
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbmZpZ3VyYXRpb24uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7OztZQU9JLCtEQUFTO1lBQ1Qsb0VBQWMiLCJmaWxlIjoiY29uZmlndXJhdGlvbi5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IEVudmlyb25tZW50IGFzIERlZmF1bHRFbnZpcm9ubWVudCB9IGZyb20gJ0Bub2QvZW52aXJvbm1lbnQnO1xuaW1wb3J0IHsgTWVyZ2VyIGFzIERlZmF1bHRNZXJnZXIgfSBmcm9tICcuL21lcmdlcic7XG5cbmV4cG9ydCBjbGFzcyBDb25maWd1cmF0aW9uIHtcblxuICBjb25zdHJ1Y3RvcihcbiAgICBvcHRpb25zLFxuICAgIE1lcmdlciA9IERlZmF1bHRNZXJnZXIsXG4gICAgRW52aXJvbm1lbnQgPSBEZWZhdWx0RW52aXJvbm1lbnRcbiAgKSB7XG5cbiAgICBsZXQgbWVyZ2VyID0gbmV3IE1lcmdlcih0aGlzLCAobmV3IEVudmlyb25tZW50KCkpLmNvbmZpZywgb3B0aW9ucyk7XG4gICAgT2JqZWN0LmFzc2lnbih0aGlzLCBtZXJnZXIub3B0aW9ucyk7XG5cbiAgICByZXR1cm4gdGhpcztcbiAgfVxufVxuIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbmZpZ3VyYXRpb24uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7OztZQVNJLCtEQUFTO1lBQ1Qsb0VBQWMiLCJmaWxlIjoiY29uZmlndXJhdGlvbi5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IEVudmlyb25tZW50IGFzIERlZmF1bHRFbnZpcm9ubWVudCB9IGZyb20gJ0Bub2QvZW52aXJvbm1lbnQnO1xuaW1wb3J0IHsgTWVyZ2VyIGFzIERlZmF1bHRNZXJnZXIgfSBmcm9tICcuL21lcmdlcic7XG5cbmV4cG9ydCBjbGFzcyBDb25maWd1cmF0aW9uIHtcblxuICBjb25maWd1cmVkID0gZmFsc2U7XG5cbiAgY29uc3RydWN0b3IoXG4gICAgb3B0aW9ucyxcbiAgICBNZXJnZXIgPSBEZWZhdWx0TWVyZ2VyLFxuICAgIEVudmlyb25tZW50ID0gRGVmYXVsdEVudmlyb25tZW50XG4gICkge1xuXG4gICAgbGV0IG1lcmdlciA9IG5ldyBNZXJnZXIodGhpcywgKG5ldyBFbnZpcm9ubWVudCgpKS5jb25maWcsIG9wdGlvbnMsIHtcbiAgICAgIGNvbmZpZ3VyZWQgOiB0cnVlXG4gICAgfSk7XG4gICAgT2JqZWN0LmFzc2lnbih0aGlzLCBtZXJnZXIub3B0aW9ucyk7XG5cbiAgICByZXR1cm4gdGhpcztcbiAgfVxufVxuIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
--- src/configuration.js
+++ src/configuration.js
@@ -3,13 +3,17 @@ import { Merger as DefaultMerger } from './merger';
 
 export class Configuration {
 
+  configured = false;
+
   constructor(
     options,
     Merger = DefaultMerger,
     Environment = DefaultEnvironment
   ) {
 
-    let merger = new Merger(this, (new Environment()).config, options);
+    let merger = new Merger(this, (new Environment()).config, options, {
+      configured : true
+    });
     Object.assign(this, merger.options);
 
     return this;
